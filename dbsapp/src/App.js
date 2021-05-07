72d24a9f8db11d89e906fc97d75636ba63cb972c
Eclipsz <lucheng257@gmail.com>
2021-05-07 11:53:00 +0800

    Transaction first commit

--- dbsapp/src/App.js
+++ dbsapp/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import NavBar from "./component/NavBar/NavBar";
+import Transaction from "./component/Transaction/Transaction";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Transaction />
     </div>
   );
 }
---
+++ dbsapp/src/component/Transaction/Transaction.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Transaction.module.css";
+
+function Transaction() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Transaction;
