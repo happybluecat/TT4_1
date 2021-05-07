f09df863c6dc591fea9082daa3e496fbd91a4320
Kenta <58523090+kentatoh@users.noreply.github.com>
2021-05-07 14:07:59 +0800

    fixed logout visuals

--- dbsapp/src/component/NavBar/NavBar.js
+++ dbsapp/src/component/NavBar/NavBar.js
@@ -1,34 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import classes from "./NavBar.module.css";
+//import AuthContext from "../store/auth-context";
 
 function NavBar() {
+  //const ctx = useContext(AuthContext);
+  const logoutHandler = () => {
+    //ctx.onLogout();
+    console.log("logout");
+  };
   return (
     <nav>
       <div className={classes["navbar-container"]}>
         <li>
           <Link to="/" className={classes["navbar-link"]}>
-            <i class="fa fa-home"></i>
+            <i className="fa fa-home"></i>
             Home
           </Link>
         </li>
         <li>
           <Link to="/Transfer" className={classes["navbar-link"]}>
-            <i class="fa fa-exchange"></i>
+            <i className="fa fa-exchange"></i>
             Transfer
           </Link>
         </li>
         <li>
           <Link to="/AccountDetails" className={classes["navbar-link"]}>
-            <i class="fa fa-university"></i>
+            <i className="fa fa-university"></i>
             Account Details
           </Link>
         </li>
         <li>
-          <Link to="/AccountDetails" className={classes["navbar-link"]}>
+          <span className={classes.logout} onClick={logoutHandler}>
             <i class="fa fa-sign-out"></i>
             Logout
-          </Link>
+          </span>
         </li>
       </div>
     </nav>
