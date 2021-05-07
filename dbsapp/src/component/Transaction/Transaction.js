f027ca37ff9881c8a72a0f314e3ac91c27630448
Eclipsz <lucheng257@gmail.com>
2021-05-07 12:19:25 +0800

    Transaction Template 1

--- dbsapp/src/component/Transaction/Transaction.js
+++ dbsapp/src/component/Transaction/Transaction.js
@@ -4,7 +4,20 @@ import classes from "./Transaction.module.css";
 function Transaction() {
   return (
     <div>
-      
+      <form method="POST">
+        <div>
+          <label>Amount in:</label>
+          <input type="number" placeholder="SGD"/>
+        </div>
+        <div>
+          <label>Recipient:</label>
+          <input type="text" placeholder="Recipient ID"/>
+        </div>
+        <div>
+          <button type="submit">Cancel</button>
+          <button type="submit">Transfer</button>
+        </div>
+      </form>
     </div>
   );
 }
