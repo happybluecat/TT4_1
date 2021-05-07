8079d86f22a87b77956529d620fc23b2965a2ace
QianLingOngg <43084761+QianLingOngg@users.noreply.github.com>
2021-05-07 12:52:26 +0800

    Login

--- dbsapp/src/App.js
+++ dbsapp/src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import LoginPage from './components/LoginPage'
+// import AuthenticatedRoute from './AuthenticatedRoute';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <LoginPage />
-      </header>
-    </div>
+    <LoginPage />
   );
 }
 
--- dbsapp/src/components/LoginPage.js
+++ dbsapp/src/components/LoginPage.js
@@ -1,19 +1,50 @@
-const LoginPage = () => {
+import { useState } from 'react'
+import axios from 'axios';
+import { setUserSession } from './api/Common';
+
+const LoginPage = (props) => {
+  const [username, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (!username ) {
+      alert('Please insert a userName / password')
+      return
+    }
+
+    setUserName('')
+    setPassword('')
+  }
+
+  const handleLogin = () => {
+    setError(null);
+    setLoading(true);
+    axios.post('http://localhost:4000/users/signin', { username: username.value, password: password.value }).then(response => {
+      setLoading(false);
+      setUserSession(response.data.token, response.data.user);
+      props.history.push('/dashboard');
+    }).catch(error => {
+      setLoading(false);
+      if (error.response.status === 401) setError(error.response.data.message);
+      else setError("Something went wrong. Please try again later.");
+    });
+  }
 
     return (
         <div>
-                <h1>Login</h1>
-                
-            <form className ='login-form' >
+            <h1>Login</h1>
+            <form className ='login-form' onSubmit={onSubmit} >
                 <div className = 'form-control'>
                     <label>Username</label>
-                    <input type ='text' placeholder='Username' ></input>
+                    <input type ='username' placeholder='Username' value={username} onChange={(e) => setUserName(e.target.value)} ></input>
                 </div>
                 <div className = 'form-control'>
                     <label>Password</label>
-                    <input type ='text' placeholder='Password'></input>
+                    <input type ='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}></input>
                 </div>
-            <input type ='submit' value = 'Login' ></input>
+            <input type ='submit' value = 'Login' onClick={handleLogin}></input>
             </form>
         </div>
     )
